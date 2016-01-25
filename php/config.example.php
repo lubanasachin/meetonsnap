80ba03b60f5c64f26acdf3d5ba2f8fbed5d2da3b
Rauno Viskus <rauno56@gmail.com>
2016-01-25 14:31:05 +0800

    fix some styling

--- php/config.example.php
+++ php/config.example.php
@@ -25,11 +25,11 @@
 			'dbusername'		=> '__?__',
 			'dbpassword'		=> '__?__',
 			'dbname'		=> 'tigasedb'
-		),		
+		),
 
 		'TWILLIO' => array(
-			'sid' 				=> "__?__",
-			'token'				=> "__?__"
+			'sid' 				=> '__?__',
+			'token'				=> '__?__'
 		),
 
 		'templateDir'			=> '../views/templates/',
@@ -42,7 +42,7 @@
 
 			'CCEMAILADDRESS'		=> '__?__',
 
-			
+
 			'resetpassword'		=> array(
 										'template' => '../views/templates/resetpassword.html',
 										'subject' => 'MOS:: Reset your password',
@@ -69,7 +69,7 @@
 			'101'				=> 'Cannot process your request at this moment',
 			'102'				=> 'Oops, something went wrong. Please retry after sometime',
 			'103'				=> 'Hey, you made an invalid request',
-			'104'				=> 'Oops, cannot load previous chat messages',			
+			'104'				=> 'Oops, cannot load previous chat messages',
 			'105'				=> 'Hey, you entered an invalid email address',
 			'106'				=> 'Hey, this email address is already registered',
 			'107'				=> 'Hey, you entered invalid email address or password',
@@ -83,7 +83,7 @@
 			'115'				=> 'File size exceeded the allowed limit of 100KB. Cannot proceed',
 			'116'				=> 'File type is invalid. Cannot proceed',
 			'117'				=> 'Hey, this email address is not registered',
-			
+
 		)
 	);
 ?>
